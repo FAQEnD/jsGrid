ccf7d1a0daf69cbac1d13f34acc7246d94e63286
Anton <hidemenowplz@gmail.com>
2015-07-05 16:49:34 +0300

    v0.4
    -Add data format;
    -Rewrite html grid.

--- app/components/gridCtrl.js
+++ app/components/gridCtrl.js
@@ -1,4 +1,42 @@
 (function() {
+
+	var dataFormats = 
+	[
+	    {
+	        "Name":"Login",
+	        "DataType":"String",
+	        "IsRequired":"true"
+	    },
+	    {
+	        "Name":"Password",
+	        "DataType":"String",
+	        "IsRequired":"false"
+	    },
+	    {
+	        "Name":"BirthDate",
+	        "DataType":"Date",
+	        "IsRequired":"false"
+	    },
+	    {
+	        "Name":"RegistrationDate",
+	        "DataType":"Date",
+	        "IsRequired":"false"
+	    },
+	    {
+	        "Name":"Age",
+	        "DataType":"Int",
+	        "IsRequired":"false",
+	        "CalculatedFrom":["BirthDate"],
+	        "Calculate":function(birthDate)
+	        {
+	            var date = new Date();
+				date.setTime(Date.parse(birthDate));
+				var age = (Date.now() - date)/(1000*60*60*24*365);
+				age = parseInt(age);
+	        }
+	    }
+	]
+
     angular.module('myApp', []).controller('gridCtrl', function($scope, $http) {
     	$http.get('users/users.json').success(function(data){
     		$scope.users = data;
