b5ef4f27561a742636e75fc8158d56d78eebc1e9
Anton <hidemenowplz@gmail.com>
2015-07-05 13:52:10 +0300

    v0.3
    -Now data get from users.json;
    -Include grid controller to index.html.

--- app/components/gridCtrl.js
+++ app/components/gridCtrl.js
@@ -1,22 +1,7 @@
 (function() {
-    angular.module('myApp', []).controller('gridCtrl', function($scope) {
-		$scope.users = [
-			{
-				name:'Anton', 
-				login:'FAQ', 
-				password:'3215had1', 
-				birthDate:'20/06/1995', 
-				registrationDate:'01/05/2015', 
-				age:10
-			},
-			{
-				name:'Jeka', 
-				login:'Ites', 
-				password:'fkak321a', 
-				birthDate:'05/01/1996', 
-				registrationDate:'01/01/2015', 
-				age:10
-			}
-		];
+    angular.module('myApp', []).controller('gridCtrl', function($scope, $http) {
+    	$http.get('users/users.json').success(function(data){
+    		$scope.users = data;
+    	});
 	})
 }());
